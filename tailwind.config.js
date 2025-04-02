0add2cb29a59f5857ac87e0a33c58778c07b822f
ADOwen <92840021+ADOwen@users.noreply.github.com>
2025-04-02 11:29:00 -0500

    fix: update tailwind content configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/contexts/**/*.{js,ts,jsx,tsx}",
+    "./src/assets/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: 'class',
   theme: {
